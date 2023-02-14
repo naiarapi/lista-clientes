2b6cb36b9d0617f6e167650a8687e17d86dae1fb
naiara <naiarapi@users.noreply.github.com>
2023-02-14 15:05:56 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,26 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  /*guardar elementos*/
+  todoArray = [];
+  /*Añadir*/
+  addTodo(value) {
+    this.todoArray.push(value);
+  }
+
+  /*eliminar*/
+
+  deleteItem(todo) {
+    for (let i = 0; i <= this.todoArray.length; i++) {
+      if ((todo = this.todoArray[i])) {
+        this.todoArray.splice(i, 1);
+      }
+    }
+  }
+
+todoSubmit(value:any){     if(value!==""){    this.todoArray.push(value.todo)     //this.todoForm.reset()    }else{      alert('Field required **')    }      }}
